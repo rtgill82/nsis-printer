c751e38d827f0484b8321c20b5b135db22b78823
Robert Gill <locke@sdf.lonestar.org>
2016-04-10 12:42:39 -0700

    Add license information

--- src/printer.c
+++ src/printer.c
@@ -1,3 +1,19 @@
+/*
+ * Created:  Fri 12 Dec 2014 07:37:55 PM PST
+ * Modified: Sun 10 Apr 2016 12:40:33 PM PDT
+ *
+ * Copyright (C) 2014-2016 Robert Gill <locke@sdf.lonestar.org>
+ *
+ * Licensed under the zlib/libpng license (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * Licence details can be found in the file COPYING.
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.
+ *
+ */
+
 #include <stdio.h>
 #include <windows.h>
 #include <winspool.h>
